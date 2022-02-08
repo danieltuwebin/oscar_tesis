821519393b20e9abbb86f68273bf0cd533c48f81
Daniel C.A <daniel@tuwebin.com>
2022-02-08 12:45:48 -0500

    Escape de ' en cadena HTML

--- Elicreditos/ajax/amortizacion.php
+++ Elicreditos/ajax/amortizacion.php
@@ -10,7 +10,7 @@ $idpersona = isset($_POST["idpersona"]) ? limpiarCadena($_POST["idpersona"]) : "
 $tipo_persona = isset($_POST["tipo_persona"]) ? limpiarCadena($_POST["tipo_persona"]) : "";
 $id_cliente = isset($_POST["id_cliente"]) ? limpiarCadena($_POST["id_cliente"]) : "";
 $documento = isset($_POST["documento"]) ? limpiarCadena($_POST["documento"]) : "";
-$numerodoc = isset($_POST["numerodoc"]) ? limpiarCadena($_POST["numerodoc"]) : "";
+$numerodoc = isset($_POST["numerodoc"]) ? limpiarCadena(strtoupper($_POST["numerodoc"])) : "";
 $fechaemision = isset($_POST["fechaemision"]) ? limpiarCadena($_POST["fechaemision"]) : "";
 $fechavencimiento = isset($_POST["fechavencimiento"]) ? limpiarCadena($_POST["fechavencimiento"]) : "";
 $tipoMoneda = isset($_POST["tipoMoneda"]) ? limpiarCadena($_POST["tipoMoneda"]) : "";
@@ -27,7 +27,7 @@ switch ($_GET["op"]) {
 		while ($reg = $rspta->fetch_object()) {
 			$data[] = array(
 				"0" => ($reg->condicion == 1) ? '<button class="btn btn-info btn-xs" onclick="mostrar(' . $reg->idamortizacion . ')"><i class="fa fa-eye"></i></button>'
-					. ' ' . '<button class="btn btn-warning btn-xs" onclick="amortizar(' . $reg->idamortizacion . ')"><i class="fa fa-pencil"></i></button>'
+					. ' ' . '<button class="btn btn-warning btn-xs" onclick="amortizar(' . $reg->idamortizacion .',&apos;'. $reg->nombre .'&apos;)"><i class="fa fa-pencil"></i></button>'
 					: '<button class="btn btn-info btn-xs" onclick="mostrar(' . $reg->idamortizacion . ')"><i class="fa fa-eye"></i></button>',
 				//"0" => $reg->idamortizacion,
 				"1" => $reg->idamortizacion,
@@ -80,5 +80,11 @@ switch ($_GET["op"]) {
 		echo json_encode($rspta);
 		//echo $rspta;
 		//echo '1234 '.$idamortizacion;
+		break;
+
+	case 'actualizarEstado':
+		$rspta = $amortizacion->actualizarEstado($idamortizacion);
+		echo $rspta ? "Datos desactivados correctamente" : "No se pudo desactivar los datos";
+		//echo $rspta;
 		break;
 }
